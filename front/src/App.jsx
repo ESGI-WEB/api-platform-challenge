1ee39d4e5b6523cf0e7ff631b5226a748bb94521
Laurie Morin <laurie20010701@gmail.com>
2023-10-22 15:22:05 +0200

    adding translations

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,8 +2,10 @@ import {useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {useTranslation} from "react-i18next";
 
 function App() {
+    const { t, i18n } = useTranslation();
     const [count, setCount] = useState(0)
     const [pong, setPong] = useState('calling backend...')
 
@@ -36,6 +38,7 @@ function App() {
     return (
         <>
             <div>
+                <h1>{t('title')}</h1>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo"/>
                 </a>
