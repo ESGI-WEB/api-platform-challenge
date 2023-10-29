4c948357901158d37592a5ea38d4e420e881b208
Laurie Morin <laurie20010701@gmail.com>
2023-10-29 14:27:15 +0100

    adding translations in caches

--- front/src/i18n.js
+++ front/src/i18n.js
@@ -1,7 +1,9 @@
-import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
+import Backend from 'i18next-chained-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
+import HttpApi from 'i18next-http-backend';
 
 i18n
     // load translation using http -> see /public/locales
@@ -17,6 +19,15 @@ i18n
     .init({
         fallbackLng: 'en',
         debug: true,
+        backend: {
+            backends: [
+                LocalStorageBackend,  // primary
+                HttpApi  // fallback
+            ],
+            backendOptions: [{
+                loadPath: '/api/translations' // http api load path
+            }],
+        },
     });
 
 export default i18n;
