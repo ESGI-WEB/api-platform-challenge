9b7961830d1e27d948decc7154d559cf3ee1b3d7
Nicolas W <nwadoux@myges.fr>
2023-11-23 19:39:45 +0100

    add id in jwt

---
+++ api/src/EventListener/JwtCreatedListener.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Doctrine\ORM\EntityRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class JwtCreatedListener
+{
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function onJwtCreated(JWTCreatedEvent $event): void
+    {
+        $user = $event->getUser();
+
+        $payload = $event->getData();
+        $payload['id'] = $user->getId();
+
+        $event->setData($payload);
+    }
+}
+
