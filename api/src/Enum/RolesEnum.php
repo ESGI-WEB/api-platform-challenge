2ca203f43395589574ee7c52fd9c77a751d73a42
Nicolas W <nwadoux@myges.fr>
2023-11-25 19:58:28 +0100

    generate migration

--- api/src/Enum/RolesEnum.php
+++ api/src/Enum/RolesEnum.php
@@ -9,5 +9,6 @@ enum RolesEnum: string
     case ADMIN = 'ROLE_ADMIN';
     case ROLE_PROVIDER_MANAGER = 'ROLE_PROVIDER_MANAGER';
     case PROVIDER = 'ROLE_PROVIDER';
+    case PROVIDER_TO_VALIDATE = 'ROLE_PROVIDER_TO_VALIDATE';
     case USER = 'ROLE_USER';
 }
